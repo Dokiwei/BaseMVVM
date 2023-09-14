ab8cd8fa7bcf5dc2e613035e6d7ea21b4dc8fe5c
DokiWei <w274643684@gmail>
2023-09-14 16:58:32 +0800

    new Constants.AnimProperty

--- app/src/main/java/com/dokiwei/basemvvm/util/Constants.kt
+++ app/src/main/java/com/dokiwei/basemvvm/util/Constants.kt
@@ -17,4 +17,30 @@ object Constants {
         data object Square : HomeViewPage("广场")
         data object Qa : HomeViewPage("问答")
     }
+
+    sealed class AnimProperty(val name: String) {
+        //透明度动画
+        data object Alpha : AnimProperty("alpha")
+
+        //旋转动画:围绕X轴旋转
+        data object RotationX : AnimProperty("rotationX")
+
+        //旋转动画:围绕Y轴旋转
+        data object RotationY : AnimProperty("rotationY")
+
+        //旋转动画:围绕z轴旋转
+        data object Rotation : AnimProperty("rotation")
+
+        //平移动画:在x轴上平移
+        data object TranslationX : AnimProperty("translationX")
+
+        //平移动画:在y轴上平移
+        data object TranslationY : AnimProperty("translationY")
+
+        //缩放动画：在x轴缩放
+        data object ScaleX : AnimProperty("scaleX")
+
+        //缩放动画：在y轴上缩放
+        data object ScaleY : AnimProperty("scaleY")
+    }
 }
